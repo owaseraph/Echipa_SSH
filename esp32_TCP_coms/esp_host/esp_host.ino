05760ca7450d486d77226143694a9dc97e100847
Robert <tarnita.robert01@gmail.com>
2026-03-13 18:55:43 +0200

    Handling serial input from FPGA

--- esp32_TCP_coms/esp_host/esp_host.ino
+++ esp32_TCP_coms/esp_host/esp_host.ino
@@ -1,14 +1,21 @@
+// host side for TCP connection
+
 #include <WiFi.h>
+#include <HardwareSerial.h>
+
 const char* ssid = "ESP32_HOST";
 const char* password = "regelepetric";
 const int port = 80;
+
 WiFiServer server(port); //listen on port 80
+HardwareSerial FPGASerial(1); // FPGA object on UART1 (UART0 is occupied by Serial)
 
 void setup(){
   Serial.begin(115200);
   delay(2000);
   Serial.println("Serial connection ready");
 
+  FPGASerial.begin(9600, SERIAL_8N1, 16,17); // FPGA uart serial
   // start the server
   WiFi.softAP(ssid, password); // create acces point
   server.begin(); // start listening for connections
@@ -26,18 +33,32 @@ void loop(){
     Serial.println("\n\tChat Started\n");
 
     while(client.connected()){
-       // send message to client
-      if(Serial.available()){
-         String hostMsg = Serial.readStringUntil('\n');
+
+       // received something from FPGA -> send to client esp
+      if(FPGASerial.available()){
+         String hostMsg = FPGASerial.readStringUntil('\n');
+         hostMsg.trim();
          client.println(hostMsg);
-         Serial.print("Host: ");
+         Serial.print("Host FPGA -> WiFi: ");
          Serial.println(hostMsg);
       }
 
-      //check for messages from client
+       // send message to client from Arduino Serial
+      if(Serial.available()){
+         String hostMsg = Serial.readStringUntil('\n');
+         hostMsg.trim();
+         client.println(hostMsg);
+         Serial.print("Serial Host -> WiFI: ");
+         Serial.println(hostMsg);
+      }
+
+      // receive something from client on WiFi -> send to FPGA
       if(client.available()){
-          Serial.print("Client: ");
-          Serial.println( client.readStringUntil('\n'));
+          Serial.print("WiFi -> Host FPGA: ");
+          String clientMsg = client.readStringUntil('\n');
+          clientMsg.trim();
+          Serial.println(clientMsg);
+          FPGASerial.println(clientMsg);
       }
     }
     Serial.println("Client disconneced");
